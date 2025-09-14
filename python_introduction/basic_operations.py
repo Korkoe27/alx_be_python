b6e21f69f30f1bf111cf9861cc8c39df41e9b172
Mechanic <dumashieanthony@gmail.com>
2025-09-14 22:24:12 +0000

    fixed type error

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 number1 = 10
 number2 = 5
 
-print("Addition of " + number1 + " and " + number2 + " is " + (number1 + number2))
-print("Subtraction of " + number2 + " and " + number1 + " is " + (number1 - number2))
-print("Multiplication of " + number1 + " and " + number2 + " is " + (number1 * number2))
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(number1 + number2))
+print("Subtraction of " + str(number1) + " from " + str(number2) + " is " + str(number1 - number2))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(number1 * number2))
